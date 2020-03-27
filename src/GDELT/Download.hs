50231ef33a81f1f0b92f33f1d2a7c7ae15ce3379
Marco Zocca <ocramz>
2020-03-27 20:05:44 +0100

    FIXME Show URLChunks

--- src/GDELT/Download.hs
+++ src/GDELT/Download.hs
@@ -1,19 +1,25 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE DataKinds #-}
 {-# language OverloadedStrings #-}
+{-# language LambdaCase #-}
 module GDELT.Download where
-
+import Control.Applicative (Alternative(..))
 import Control.Monad (void)
 import Data.Functor (($>))
+import Data.Void (Void)
 
 -- megaparsec
-import Text.Megaparsec
-import Text.Megaparsec.Char (string)
+import Text.Megaparsec (Parsec, ParseErrorBundle, parse, parseTest, count, eof)
+import Text.Megaparsec.Char (string, char, digitChar)
+import Text.Megaparsec.Char.Lexer (decimal)
 -- req
 import Network.HTTP.Req (MonadHttp(..), req, http, (/:), GET, NoReqBody, bsResponse, Url, Scheme(..))
 -- text
 import Data.Text (Text, pack, unpack)
 import Data.Text.Encoding (decodeUtf8)
+-- time
+import Data.Time.LocalTime (LocalTime(..), TimeOfDay(..))
+import Data.Time.Calendar (Day, fromGregorian)
 
 
 {-
@@ -29,17 +35,88 @@ e.g.
 lastUpdateUrl :: Url 'Http
 lastUpdateUrl = http "data.gdeltproject.org" /: "gdeltv2" /: "lastupdate.txt"
 
-data GDDataset a = GDExport a
-                 | GDMentions a
-                 | GDGKG a
-                 deriving (Eq, Show, Functor)
+data GDSource = GDSource {
+    gdsHash :: Text
+  , gdsUrl :: GDDataset
+  } deriving (Eq, Show)
 
-data GDSource a = GDSource { gdsSize :: Int, gdsHash :: Text, gdsType :: GDDataset a } deriving (Eq, Show, Functor)
+data GDDataset = GDDataset {
+  gdDate :: LocalTime
+  , gdTy :: GDDatasetTy } deriving (Eq, Show)
+
+data GDDatasetTy = GDTExport
+                 | GDTMentions
+                 | GDTGKG 
+                 deriving (Eq)
+instance Show GDDatasetTy where
+  show = \case
+    GDTExport -> "export"
+    GDTMentions -> "mentions"
+    GDTGKG -> "gkg"
+
+data URLChunks d = URLChunks { ucPrefix :: Text, ucDate :: d, ucDTy :: GDDatasetTy, ucExt :: Text } deriving (Eq)
+instance Show d => Show (URLChunks d) where
+  show (URLChunks spre sd sty se) = unpack spre <> show sd <> "." <> show sty <> unpack se
+ 
+chunkUrl :: Parser (URLChunks String)
+chunkUrl = do
+  pref <- string "http://data.gdeltproject.org/gdeltv2/"
+  ymd <- count 14 digitChar
+  void $ char '.'
+  ty <- (string "export" $> GDTExport ) <|> (string "gkg" $> GDTGKG) <|> (string "mentions" $> GDTMentions)
+  ext <- string ".csv.zip" <|> string ".CSV.zip"
+  pure $ URLChunks pref ymd ty ext
+
+example0 :: Text
+example0 = "http://data.gdeltproject.org/gdeltv2/20200327131500.gkg.csv.zip"
+example1 :: Text
+example1 = "http://data.gdeltproject.org/gdeltv2/20200327131500.mentions.CSV.zip"
+
+-- gdsUrlP :: Parser (GDDatasetTy Text)
+-- gdsUrlP = do
+--   pref <- string "http://data.gdeltproject.org/gdeltv2/"
+--   ymd <- count 14 digitChar
+--   void $ char '.'
+--   (xsf, s) <- parseTy
+--   ext <- string ".csv.zip" <|> string ".CSV.zip"
+--   pure $ xsf (pref <> pack ymd <> "." <> s <> ext)
+
+type Parser = Parsec Void Text
+
+type ParseError = ParseErrorBundle Text Void
+
+
+-- parseYMD :: Parser Int
+-- parseYMD = count 4 digitChar >>= decimal
+
+
+parseDate :: Text -> Either ParseError LocalTime
+parseDate z = do
+  (yys, mms, dds, hs, ms, ss) <- parse chunkDate "" z
+  yy <- parse decimal "" yys
+  mm <- parse decimal "" mms
+  dd <- parse decimal "" dds
+  let day = fromGregorian yy mm dd
+  h <- parse decimal "" hs
+  m <- parse decimal "" ms
+  s <- parse decimal "" ss
+  let tod = TimeOfDay h m (fromIntegral s)
+  pure $ LocalTime day tod
+
+chunkDate :: Parser (Text, Text, Text, Text, Text, Text)
+chunkDate = do
+  yys <- count 4 digitChar
+  mms <- count 2 digitChar
+  dds <- count 2 digitChar
+  hs <- count 2 digitChar
+  ms <- count 2 digitChar
+  ss <- count 2 digitChar
+  pure (pack yys, pack mms, pack dds, pack hs, pack ms, pack ss)
+
+-- parseTy :: Parser ((Text -> GDDatasetTy Text), Text)
+-- parseTy = (string "export" $> (GDTExport, "export")) <|>
+--           (string "mentions" $> (GDTMentions, "mentions")) <|>
+--           (string "gkg" $> (GDTGKG, "gkg"))
 
 
 
--- http://data.gdeltproject.org/gdeltv2/20200327131500.gkg.csv.zip
-
--- gkgP pref = do
---   p <- string pref
---   void
