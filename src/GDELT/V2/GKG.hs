1cfb26f42e8bd360c6c4f634c66f82d868673576
ocramz <zocca.marco@gmail.com>
2020-03-31 09:09:58 +0200

    release 0.1

--- src/GDELT/V2/GKG.hs
+++ src/GDELT/V2/GKG.hs
@@ -1,7 +1,15 @@
 {-# language DeriveGeneric #-}
 {-# language OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
-{-|
+
+{- |
+Module      :  GDELT.V2.GKG
+Description :  Parsers for GDELT  Global  Knowledge  Graph  (GKG) Version  2.1
+Copyright   :  (c) Marco Zocca 2020
+License     :  MIT
+Maintainer  :  ocramz
+Stability   :  experimental
+Portability :  portable
 
 Data : http://data.gdeltproject.org/gkg/index.html (2013 until present)
 
@@ -10,6 +18,7 @@ Codebook : http://data.gdeltproject.org/documentation/GDELT-Global_Knowledge_Gra
 This  codebook  introduces  the  GDELT  Global  Knowledge  Graph  (GKG) Version  2.1,  which  expands GDELT’s ability to quantify global human society beyond cataloging physical occurrences towards actually representing all of the latent dimensions, geography, and network structure of the global news.  It  applies  an array  of  highly  sophisticated  natural  language  processing  algorithms to  each  document  to compute a range of codified metadata encoding key latent and contextual dimensions of the document.  To sum up the GKG in a single sentence, it connects every person, organization, location, count, theme, news source, and event across the planet into a single massive network that captures what’s happening around the world, what its context is and who’s involved, and how the world is feeling about it, every single day.
 
 -}
+
 module GDELT.V2.GKG (
   -- * RecordId
   RecordId(..), recordId
@@ -61,6 +70,7 @@ data RecordId = RecordId {
   , riSeqNo :: Int
   } deriving (Eq, Show, Generic)
 
+-- | Parser for a 'RecordId'
 recordId :: Parser RecordId
 recordId = do
   t <- localTime
@@ -104,6 +114,7 @@ data SourceCollectionIdentifier = SCIWeb
                                 | SCINonTextualSource
                                 deriving (Eq, Show, Enum, Generic)
 
+-- | Parser for a 'SourceCollectionIdentifier'
 sourceCollectionIdentifier :: Parser SourceCollectionIdentifier
 sourceCollectionIdentifier = toEnum . subtract 1 <$> digit
 
@@ -159,7 +170,6 @@ data CountsV1 = CountsV1 {
   } deriving (Eq, Show, Generic)
 
 
-
 data CountTy = CTAffect | CTArrest | CTKidnap | CTKill | CTProtest | CTSeize | CTWound | CTOther Text deriving (Eq, Show, Generic)
 
 countTy :: Parser CountTy
@@ -172,6 +182,7 @@ countTy = (string "AFFECT" $> CTAffect) <|>
   (string "WOUND" $> CTWound)
   -- (string "AFFECT" $> CTAffect)
 
+-- | Parser for a 'CountsV1'
 countsV1 :: Parser CountsV1
 countsV1 = do
   cty <- countTy
@@ -187,17 +198,15 @@ countsV1 = do
 
 
 
-{-|
-V2.1COUNTS
-
-(semicolon-delimited blocks, with pound symbol (“#”) delimited fields)  This field is identical to the V1COUNTS field except that it adds a final additional field to the end of each entry that records its approximate character offset in the document, allowing it to be associated with other entries from other “V2ENHANCED” fields (or Events) that appear in closest proximity to  it.   Note:unlike  the  other  location-related  fields,  the  Counts  field  does  NOT  add  ADM2 support at this time.  This is to maintain compatibility with assumptions that many applications make  about  the  contents  of  the  Count  field.    Those  applications  needing  ADM2  support  for Counts should cross-reference the FeatureID field of a given Count against the V2Locations field to determine its ADM2 value.
--}
-
+-- | V2.1COUNTS
+-- 
+-- (semicolon-delimited blocks, with pound symbol (“#”) delimited fields)  This field is identical to the V1COUNTS field except that it adds a final additional field to the end of each entry that records its approximate character offset in the document, allowing it to be associated with other entries from other “V2ENHANCED” fields (or Events) that appear in closest proximity to  it.   Note:unlike  the  other  location-related  fields,  the  Counts  field  does  NOT  add  ADM2 support at this time.  This is to maintain compatibility with assumptions that many applications make  about  the  contents  of  the  Count  field.    Those  applications  needing  ADM2  support  for Counts should cross-reference the FeatureID field of a given Count against the V2Locations field to determine its ADM2 value.
 data CountsV21 = CountsV21 {
     c21countsV1 :: CountsV1
   , c21charOffset :: Int
                            } deriving (Eq, Show, Generic)
 
+-- | Parser for a 'CountsV21'
 countsV21 :: Parser CountsV21
 countsV21 = do
   c1 <- countsV1
@@ -242,21 +251,21 @@ data ThemesV2
 {-|
 V1LOCATIONS
 
-(semicolon-delimited blocks, with pound symbol (“#”) delimited fields)  This is a list  of  all  locations  found  in  the  text,  extracted  through  the  Leetaru  (2012)  algorithm. 2The algorithm  is  run  in  a  more  aggressive  stance  here  than  ordinary  in  order  to extract  every possible locative  referent, so may have  a slightly elevated level of false  positives.  NOTE:some locations have multiple accepted formal or informal names and this field is collapsed on name, rather than feature (since in some applications the understanding of a geographic feature differs based  on  which  name  was  used  to  reference  it).    In  cases  where  it  is  necessary  to  collapse  by feature, the Geo_FeatureID column should be used, rather than the Geo_Fullname column.  This is because the Geo_Fullname column captures the name of the location as expressed in the text and  thus  reflects  differences  in  transliteration,  alternative  spellings,  and  alternative  names  for the  same  location.    For  example,  Mecca  is  often  spelled  Makkah,  while  Jeddah  is  commonly spelled  Jiddah  or  Jaddah.    The  Geo_Fullname  column  will  reflect  each  of  these  different spellings, while the Geo_FeatureID column will resolve them all to the same unique GNS or GNIS feature  identification  number.    For  more  information  on  the  GNS  andGNIS  identifiers,  see Leetaru (2012). 3This field is identical in format and population as the corresponding field in the GKG  1.0 format.NOTE:there  was  an  error in  this  field  from  2/19/2015  through midday 3/1/2015that caused the CountryCode field to listthe wrong country code in somecases.
+(semicolon-delimited blocks, with pound symbol (“#”) delimited fields)  This is a list  of  all  locations  found  in  the  text,  extracted  through  the  Leetaru  (2012)  algorithm. 2The algorithm  is  run  in  a  more  aggressive  stance  here  than  ordinary  in  order  to extract  every possible locative  referent, so may have  a slightly elevated level of false  positives.  NOTE:some locations have multiple accepted formal or informal names and this field is collapsed on name, rather than feature (since in some applications the understanding of a geographic feature differs based  on  which  name  was  used  to  reference  it).    In  cases  where  it  is  necessary  to  collapse  by feature, the Geo_FeatureID column should be used, rather than the Geo_Fullname column.  This is because the Geo_Fullname column captures the name of the location as expressed in the text and  thus  reflects  differences  in  transliteration,  alternative  spellings,  and  alternative  names  for the  same  location.    For  example,  Mecca  is  often  spelled  Makkah,  while  Jeddah  is  commonly spelled  Jiddah  or  Jaddah.    The  Geo_Fullname  column  will  reflect  each  of  these  different spellings, while the Geo_FeatureID column will resolve them all to the same unique GNS or GNIS feature  identification  number.    For  more  information  on  the  GNS  andGNIS  identifiers,  see Leetaru (2012). 3This field is identical in format and population as the corresponding field in the GKG  1.0 format. NOTE:there  was  an  error in  this  field  from  2/19/2015  through midday 3/1/2015 that caused the CountryCode field to listthe wrong country code in somecases.
 
-- Location Type.  (integer) This field specifies the geographic resolution of the match type and  holds  one  of  the  following  values:    1=COUNTRY  (match  was  at  the  country  level), 2=USSTATE  (match was to a US state), 3=USCITY  (match was  to a US city or landmark), 4=WORLDCITY  (match  was  to  a  city  or  landmark  outside  the  US),  5=WORLDSTATE (match was to an Administrative Division 1 outside the US –roughly equivalent to a US state).    This  can  be  used  to  filter  counts  by  geographic  specificity,  for  example, extracting  only  those  counts  with  a  landmark-level  geographic  resolution  for  mapping.  Note that matches with codes 1 (COUNTRY), 2 (USSTATE), and 5 (WORLDSTATE) will still provide a latitude/longitude pair, which will be the centroid of that country or state, but the  FeatureID  field  below  will  contain  its  textual  country  or  ADM1  code  instead  of  a numeric featureid.
+- Location Type.  (integer) This field specifies the geographic resolution of the match type and  holds  one  of  the  following  values:    1=COUNTRY  (match  was  at  the  country  level), 2=USSTATE  (match was to a US state), 3=USCITY  (match was  to a US city or landmark), 4=WORLDCITY  (match  was  to  a  city  or  landmark  outside  the  US),  5=WORLDSTATE (match was to an Administrative Division 1 outside the US –roughly equivalent to a US state).    This  can  be  used  to  filter  counts  by  geographic  specificity,  for  example, extracting  only  those  counts  with  a  landmark-level  geographic  resolution  for  mapping.  Note that matches with codes 1 (COUNTRY), 2 (USSTATE), and 5 (WORLDSTATE) will still provide a latitude-longitude pair, which will be the centroid of that country or state, but the  FeatureID  field  below  will  contain  its  textual  country  or  ADM1  code  instead  of  a numeric featureid.
 
 - Location  FullName.(text)  This  is  the  full  human-readable  name  of  the  matched location.  In the case of a country it is simply the country name.  For US and World states it is in the format of “State, Country Name”, while for all other matches it is in the format  of “City/Landmark, State, Country”.  This can be used to label locations when placing counts on a map.  Note: this field reflects the precise name used to refer to the location in the text itself, meaning it may contain multiple spellings of the same location –use the FeatureID column to determine whether two location names refer to the same place.
 
 - Location  CountryCode.    (text)  This  is  the  2-character  FIPS10-4  country  code  for  the location.   Note:GDELT  continues  to  use  the  FIPS10-4  codes  under  USG  guidance while GNS  continues  its  formal  transitionto the  successor  Geopolitical  Entities,  Names,  and Codes (GENC) Standard (the US Government profile of ISO 3166).
 
-- Location  ADM1Code.    (text)  This  is  the  2-character  FIPS10-4  country  code  followed  by the  2-character  FIPS10-4  administrative  division  1  (ADM1)  code  for  the  administrative division housing the landmark.  In the case  of the United States,  this is the 2-character shortform of the state’s name (such as “TX” for Texas).Note:see the notice above for CountryCode regarding the FIPS10-4 / GENC transition.  Note: to obtain ADM2 (district-level) assignments for locations, you can either perform a spatial join against a ShapeFile template in any GIS software, or cross-walk the FeatureID to the GNIS/GNS databases –this  will  provide  additional  fields  such as  ADM2  codes  and MGRS  grid  references for GNS.
+- Location  ADM1Code.    (text)  This  is  the  2-character  FIPS10-4  country  code  followed  by the  2-character  FIPS10-4  administrative  division  1  (ADM1)  code  for  the  administrative division housing the landmark.  In the case  of the United States,  this is the 2-character shortform of the state’s name (such as “TX” for Texas). Note : see the notice above for CountryCode regarding the FIPS10-4 - GENC transition.  Note: to obtain ADM2 (district-level) assignments for locations, you can either perform a spatial join against a ShapeFile template in any GIS software, or cross-walk the FeatureID to the GNIS-GNS databases –this  will  provide  additional  fields  such as  ADM2  codes  and MGRS  grid  references for GNS.
 
-- Location Latitude.  (floating point number) This is the centroid latitude of the landmark for  mapping.In  the  case  of  a  country  or  administrative  division  this  will  reflect  the centroid of that entire country/division.
+- Location Latitude.  (floating point number) This is the centroid latitude of the landmark for  mapping.In  the  case  of  a  country  or  administrative  division  this  will  reflect  the centroid of that entire country resp. division.
 
-- Location  Longitude.    (floating  point  number)  This  is  the  centroid  longitude  of  the landmark for mapping.In the case of a country or administrative division this will reflect the centroid of that entire country/division.
+- Location  Longitude.    (floating  point  number)  This  is  the  centroid  longitude  of  the landmark for mapping.In the case of a country or administrative division this will reflect the centroid of that entire country resp. division.
 
-- Location FeatureID.  (text OR signed integer) This is the numeric GNS or GNIS FeatureID for this locationOR a textual country or ADM1 code.  More information onthese values can be found in Leetaru (2012).4Note: This field will be blank or contain a textual ADM1 code  for country or ADM1-level matches –see  above.  Note: For numeric GNS or GNIS FeatureIDs,  this field  can  contain  both  positive  and  negative  numbers,  see  Leetaru (2012) for more information on this.
+- Location FeatureID.  (text OR signed integer) This is the numeric GNS or GNIS FeatureID for this location OR a textual country or ADM1 code.  More information on these values can be found in Leetaru (2012).  Note : This field will be blank or contain a textual ADM1 code  for country or ADM1-level matches –see  above.  Note: For numeric GNS or GNIS FeatureIDs,  this field  can  contain  both  positive  and  negative  numbers,  see  Leetaru (2012) for more information on this.
 -}
 
 data LocationV1 = LocationV1 {
@@ -269,6 +278,7 @@ data LocationV1 = LocationV1 {
   , locFeatureId :: Either Text Int
   } deriving (Eq, Show, Generic)
 
+-- | Parser for a 'LocationV1'
 locationV1 :: Parser LocationV1
 locationV1 = do
   lty <- locationTy
@@ -308,6 +318,8 @@ featureId = (Left . pack <$> count 2 letterChar) <|>
 
 
 {-|
-V2ENHANCEDLOCATIONS.    (semicolon-delimited blocks, with pound symbol (“#”) delimited fields)    This  field  is  identical  to  the  V1LOCATIONS  field  with  the primary exception  of  an  extra field  appended  to  the end  of  each  location  block  after  its  FeatureID  that  lists  the  approximate character  offset  of the  reference  to  that  location  in the  text.    In  addition,  if  a  location  appears multiple  times  in  the  article,  it  will  be  listed  multiple  times  in  this  field.The  only  other modification from V1LOCATIONS is the addition of a single new field “Location ADM2Code” in between “Location ADM1Code” and “Location Latitude”. 5NOTE:there was an error in this field from  2/19/2015 through midday 3/1/2015that  caused  the  CountryCode  field to listthe wrong country code in somecases.
+V2ENHANCEDLOCATIONS.    (semicolon-delimited blocks, with pound symbol (“#”) delimited fields)    This  field  is  identical  to  the  V1LOCATIONS  field  with  the primary exception  of  an  extra field  appended  to  the end  of  each  location  block  after  its  FeatureID  that  lists  the  approximate character  offset  of the  reference  to  that  location  in the  text.    In  addition,  if  a  location  appears multiple  times  in  the  article,  it  will  be  listed  multiple  times  in  this  field. The  only  other modification from V1LOCATIONS is the addition of a single new field “Location ADM2Code” in between “Location ADM1Code” and “Location Latitude”.
+
+- NOTE : there was an error in this field from  2/19/2015 through midday 3/1/2015 that  caused  the  CountryCode field to list the wrong country code in some cases.
 -}
 
